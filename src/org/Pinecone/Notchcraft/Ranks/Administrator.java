2a8076b18b705670fd0c52c7939f6969ffc2b26c
Tyler <buildcarter8@users.noreply.github.com>
2016-06-21 18:11:26 -0400

    Delete Administrator.java

--- src/org/Pinecone/Notchcraft/Ranks/Administrator.java
+++
@@ -1,9 +0,0 @@
-package org.pinecone.Notchcraft.Ranks;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class Administrator
-{
-	public static List<String> admins = Arrays.asList("Alpha_Wolf77", "_DrFluffy_", "dado163");
-}
