92c9e143110101f6d40fbb9f6cd28a27f1960d5f
Tyler <buildcarter8@users.noreply.github.com>
2016-06-20 22:53:06 -0400

    Update Command_nc.java

--- src/org/Pinecone/Notchcraft/Commands/Command_nc.java
+++ src/org/Pinecone/Notchcraft/Commands/Command_nc.java
@@ -1,18 +1,19 @@
 package org.pinecone.Notchcraft.Commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.pinecone.Notchcraft.Main;
 
-import net.md_5.bungee.api.ChatColor;
-
 public class Command_nc extends NC_Command
 {
 
-	@Override
 	public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole) {
+		
+		
 		sender.sendMessage(ChatColor.GOLD + "Notchcraft The Plugin Version " + Main.VERSION);
+		sender.sendMessage(ChatColor.AQUA + "By buildcarter8");
 		
 		
 		return true;
