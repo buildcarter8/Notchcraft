5dedefc85197cb04020de95fbc2b943a0aaa187f
Tyler <buildcarter8@users.noreply.github.com>
2016-06-21 17:42:49 -0400

    Update ServerListener.java

--- src/org/Pinecone/Notchcraft/Listeners/ServerListener.java
+++ src/org/Pinecone/Notchcraft/Listeners/ServerListener.java
@@ -1,5 +1,7 @@
 package org.pinecone.Notchcraft.Listeners;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
@@ -7,9 +9,10 @@ import net.md_5.bungee.api.ChatColor;
 
 public class ServerListener implements Listener
 {
+	@EventHandler(priority = EventPriority.HIGHEST)
 	 public void onServerPing(ServerListPingEvent event)
 	    {
 		 event.setMotd(ChatColor.GOLD + "[Notchcraft]" + ChatColor.WHITE + " -" + ChatColor.DARK_RED + "REAL ROLEPLAY");
-		 
+		 return;
 	    }
 }
