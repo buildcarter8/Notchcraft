6f700b196980658f8c19bb564145b174865de11e
Tyler <buildcarter8@users.noreply.github.com>
2016-06-20 23:30:34 -0400

    Create PlayerListener.java

---
+++ src/org/Pinecone/Notchcraft/Listeners/PlayerListener.java
@@ -0,0 +1,33 @@
+package org.pinecone.Notchcraft.Listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.pinecone.Notchcraft.Ranks.Administrator;
+import org.pinecone.Notchcraft.Ranks.Executive;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class PlayerListener implements Listener
+{
+	@EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerLogin(PlayerLoginEvent event)
+    {
+		Player player = event.getPlayer();
+		if(Executive.executive.contains(player.getName()))
+		{
+			 Bukkit.broadcastMessage(ChatColor.AQUA + player.getName() + " is an " + ChatColor.RED + "System Admin");
+		}
+		if(Administrator.admins.contains(player.getName()))
+				{
+			     Bukkit.broadcastMessage(ChatColor.AQUA + player.getName() + " is an " + ChatColor.GOLD + "Admin");
+				}
+		if(player.getName().equalsIgnoreCase("buildcarter8"))
+		{
+			 Bukkit.broadcastMessage(ChatColor.AQUA + player.getName() + " is the " + ChatColor.DARK_RED + "Owner");
+		}
+    }
+}
