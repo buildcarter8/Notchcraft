6510fd8a003cbc08c37a78e993d869cf1ecd5203
Tyler <buildcarter8@users.noreply.github.com>
2016-06-21 18:12:13 -0400

    One big ranks file

---
+++ src/org/Pinecone/Notchcraft/Ranks/Ranks.java
@@ -0,0 +1,12 @@
+package org.pinecone.Notchcraft.Ranks;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ranks 
+{
+	
+	public static List<String> executive = Arrays.asList("Tapster", "Gobomo", "Herouran");
+	public static List<String> admins = Arrays.asList("Alpha_Wolf77", "_DrFluffy_", "dado163");
+	public static List<String> staff = Arrays.asList("Tapster", "Gobomo", "Herouran", "Alpha_Wolf77", "_DrFluffy_", "dado163", "buildcarter8");
+}
