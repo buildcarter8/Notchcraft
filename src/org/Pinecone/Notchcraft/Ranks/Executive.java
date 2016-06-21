18e50f6434439601fb176cf6e1004c443b1aa41d
Tyler <buildcarter8@users.noreply.github.com>
2016-06-20 23:29:59 -0400

    Create Executive.java

---
+++ src/org/Pinecone/Notchcraft/Ranks/Executive.java
@@ -0,0 +1,9 @@
+package org.pinecone.Notchcraft.Ranks;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Executive 
+{
+	public static List<String> executive = Arrays.asList("Tapster", "Gobomo");
+}
