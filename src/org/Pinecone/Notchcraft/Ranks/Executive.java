0d79190cbd1603553db3377839e1e5004658b45b
Tyler <buildcarter8@users.noreply.github.com>
2016-06-21 18:11:10 -0400

    Delete Executive.java

--- src/org/Pinecone/Notchcraft/Ranks/Executive.java
+++
@@ -1,9 +0,0 @@
-package org.pinecone.Notchcraft.Ranks;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class Executive 
-{
-	public static List<String> executive = Arrays.asList("Tapster", "Gobomo", "Herouran");
-}
